b9ffa0af0caf3942a9d500a7ff15ff803a78f264
HoangPi <voduchoangphi@gmail.com>
2025-04-17 12:50:29 +0700

    convert into long long

--- types_of_pointers/casted_pointer/char_pointer.cpp
+++ types_of_pointers/casted_pointer/char_pointer.cpp
@@ -1,9 +1,9 @@
 #include <stdio.h>
 
 int main(){
-    __int64_t x = 0x217265746e696f50;
+    long long x = 0x217265746e696f50;
     char *ptr = (char *)&x;
-    for (int i = 0; i < sizeof(__int64_t); i++)
+    for (int i = 0; i < sizeof(long long); i++)
     {
         printf("%c", ptr[i]);
     }
