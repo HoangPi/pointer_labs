57e73a73a3531a0a3c19ddfdd6c7417f4dd6f6ab
HoangPi <voduchoangphi@gmail.com>
2025-04-18 11:24:16 +0700

    replace the const with "size"

--- usage/e2_dynamic_initialization.cpp
+++ usage/e2_dynamic_initialization.cpp
@@ -19,6 +19,6 @@ int find_max(int *arr, int size){
 // As long as there is room for it
 int main(){
     int size = 5;
-    int *arr = new int[3]{1,55,3};
+    int *arr = new int[size]{1,55,3, 7, 11};
     printf("Max is %d\n", find_max(arr, size));
 }
