2053f04b20c83eafe19dd65332cefddcde6b933d
HoangPi <voduchoangphi@gmail.com>
2025-04-18 12:57:14 +0700

    add ASCII note

--- lab2_types_of_pointer/casted_pointer/char_pointer_point_to_int64.cpp
+++ lab2_types_of_pointer/casted_pointer/char_pointer_point_to_int64.cpp
@@ -1,6 +1,15 @@
 #include <stdio.h>
 
 int main(){
+    // ASCII code
+    // 0x21 = !
+    // 0x72 = r
+    // 0x65 = e
+    // 0x74 = t
+    // 0x6e = n
+    // 0x69 = i
+    // 0x6f = o
+    // 0x50 = P
     long long x = 0x217265746e696f50;
     char *ptr = (char *)&x;
     for (int i = 0; i < sizeof(long long); i++)
