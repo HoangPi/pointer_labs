71fab29eef3ad6f8277e2f3e4994e21be227d818
Phi <voduchoangphi@gmail.com>
2025-04-18 14:10:55 +0700

    add note

--- lab3_problems/dangling_pointer/dangling.cpp
+++ lab3_problems/dangling_pointer/dangling.cpp
@@ -4,8 +4,9 @@ int main(){
     int *x = new int(8);
     printf("Value at x is %d\n", *x);
     delete x;
-    // g++ will continue but msvc will explode and die
-    printf("Value at x is %d\n", *x);
+    printf("Value at x is %d\n", *x);   // This will give you undefined result
+    // 44 will occupy the old space because that space is freshly freed so why not
+    // even if you don't re-asign the pointer
     new int(44);
     printf("Value at x is %d\n", *x);
 }
